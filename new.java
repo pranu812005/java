5c2dd0635a8819eb39c6ffe1003ac5c13d146e5c
pranu812005 <pranalibhandare02@gmail.com>
2026-02-25 11:21:41 +0530

    Update new.java

--- new.java
+++ new.java
@@ -1,8 +1,9 @@
 public class HelloWorld {
-    public static void main( string[] args) {
+    public static void main( string[]args) {
         System.out.println("Hello, cloud batch1");
     }
 
 }
+
 
 
